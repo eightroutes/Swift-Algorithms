d0e6d8fec33e92379f6582ddc0a5298494d41a3d
EllGHT <rmsgh1188@gmail.com>
2025-10-09 17:20:13 +0900

    feat: 주차요금계산
    통과

--- Algorithms/Programmers/주차요금계산.swift
+++ Algorithms/Programmers/주차요금계산.swift
@@ -3,7 +3,7 @@
 //  Algorithms
 //
 //  Created by 정근호 on 10/8/25.
-//
+// https://school.programmers.co.kr/learn/courses/30/lessons/92341
 
 import Foundation
 
@@ -15,43 +15,91 @@ func parkingFee(_ fees:[Int], _ records:[String]) -> [Int] {
     // records
     // 시:분 번호 in/out
     
-    let defaultMinute = fees[0]
-    let defaultFee = fees[1]
-    let unitMinute = fees[2]
-    let unitFee = fees[3]
+    //    var parkingTimes = [Int]()
+    //
+    //    var records = records.map {
+    //        $0.components(separatedBy: " ")
+    //    }
+    //
+    //    records.sort {
+    //        Int($0[1])! < Int($1[1])!
+    //    }
+    //    print(records)
+    //
+    //    var totalMinute = 0
+    //
+    //    for i in 0..<records.count-1 {
+    //        if records[i][1] == records[i+1][1] {
+    //
+    //            totalMinute +=
+    //            recordToMinute(record: records[i+1][0])
+    //            - recordToMinute(record: records[i][0])
+    //
+    //            if records[i+1][2] == "IN" {
+    //                let minuteLast = ((23*60)+59) - recordToMinute(record: records[i+1][0])
+    //                totalMinute += minuteLast
+    //            }
+    //        } else {
+    //            parkingTimes.append(totalMinute)
+    //            totalMinute = 0
+    //        }
+    //    }
+    //
+    //    print(parkingTimes)
     
-    var parkingTimes = [Int]()
+    var inRecords = [String:Int]() // 차 번호 : 시간
+    var totalTimes = [String:Int]() // 차 번호 : 총 주차시간
     
-    var records = records.map {
-        $0.components(separatedBy: " ")
-    }
-    
-    records.sort {
-        Int($0[1])! < Int($1[1])!
-    }
-    print(records)
-    
-    var totalMinute = 0
+    for record in records {
+        let parts = record.components(separatedBy: " ")
+        let time = parts[0]
+        let carNum = parts[1]
+        let action = parts[2]
+        let minute = recordToMinute(record: time)
         
-    for i in 0..<records.count-1 {
-        if records[i][1] == records[i+1][1] {
-            
-            totalMinute +=
-            recordToMinute(record: records[i+1][0])
-            - recordToMinute(record: records[i][0])
-            
-            if records[i+1][2] == "IN" {
-                let minuteLast = ((23*60)+59) - recordToMinute(record: records[i+1][0])
-                totalMinute += minuteLast
-            }
+        if action == "IN" {
+            inRecords[carNum] = minute
         } else {
-            parkingTimes.append(totalMinute)
-            totalMinute = 0
+            if let start = inRecords[carNum] {
+                totalTimes[carNum, default: 0] += (minute - start)
+                inRecords.removeValue(forKey: carNum)
+            }
         }
     }
     
-    print(parkingTimes)
-    return []
+    // OUT 안한 차량 있을 시 23:59에 OUT 처리
+    if !inRecords.isEmpty {
+        for (carNum, minute) in inRecords {
+            totalTimes[carNum, default: 0] += (23*60+59 - minute)
+        }
+    }
+    
+    print(totalTimes)
+    
+    func calculateFee(_ minute: Int) -> Int {
+        let defaultMinute = fees[0]
+        let defaultFee = fees[1]
+        let unitMinute = fees[2]
+        let unitFee = fees[3]
+        
+        if minute <= defaultMinute {
+            return defaultFee
+        } else {
+            let val = Float(minute - defaultMinute) / Float(unitMinute)
+            return defaultFee + Int(ceil(val)) * unitFee
+        }
+    }
+    
+    let sortedCarNumb = totalTimes.keys.sorted()
+    print(sortedCarNumb)
+    
+    let sortedTotalTimes = sortedCarNumb.map {
+        return calculateFee(totalTimes[$0]!)
+    }
+    
+    print(sortedTotalTimes)
+    
+    return sortedTotalTimes
 }
 
 
@@ -63,3 +111,4 @@ func recordToMinute(record: String) -> Int {
     let minute = (time[0]*60 + time[1])
     return minute
 }
+
